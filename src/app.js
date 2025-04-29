c2c6b5b819c94719bd4cac6e522b8e7cc809557c
Marjoriecus <101658277+Marjoriecus@users.noreply.github.com>
2025-04-29 00:34:08 +0000

    fixed final from local

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".net", ".us", ".com"];
+
+  for (let x of pronoun) {
+    for (let y of adj) {
+      for (let z of noun) {
+        for (let w of domain)
+          document.querySelector("#dom").innerHTML +=
+            "<h3>" + x + y + z + w + "</h3>";
+      }
+    }
+  }
 };
